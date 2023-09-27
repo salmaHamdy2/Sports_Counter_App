805e00325f7677ca18298f6889b68df2937249e4
salma hamdy <74612024+salmaHamdy2@users.noreply.github.com>
2023-09-28 02:47:38 +0300

    cleanArci3

--- lib/Screens/score.dart
+++ lib/Screens/score.dart
@@ -1,5 +1,5 @@
 
-import 'package:basketball_points_counter_app/components/column.dart';
+import 'package:basketball_points_counter_app/Components/column.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
